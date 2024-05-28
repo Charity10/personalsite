2f81ed2328a05068b3b8e104454e79c6750cfe77
Charity <umercy001@gmail.com>
2024-05-29 00:14:34 +0100

    added files

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,19 @@
+
+
+function Navbar() {
+
+  return (
+     <div className="navbar">
+      <ul className="nav-list">
+        <li><a href="#">Mercy Umoh</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Achievment</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+     </div>
+  )
+}
+
+
+export default Navbar;
